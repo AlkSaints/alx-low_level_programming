d357e7ace3f0b7b88ffc8b8e358eab624dd8b334
AlkSaints <meshacksuter2@gmail.com>
2022-12-09 12:08:48 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: reverse alphabet
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
