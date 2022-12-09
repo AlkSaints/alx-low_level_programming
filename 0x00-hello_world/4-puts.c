9b5ec2ebf150ab31c18541d42be0f55952f33aea
AlkSaints <meshacksuter2@gmail.com>
2022-12-08 20:17:03 -0800

    description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main entry
- * return is always 0
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
