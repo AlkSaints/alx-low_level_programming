94fa58dad3235a02e1aca1a3d19c08a0f095b206
AlkSaints <meshacksuter2@gmail.com>
2022-12-09 12:22:17 -0800

    numbers to base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Numbers to base 16 in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (c = 'a'; c < 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
