2ce309dacc0106a8fe9b3f4e696431aa49e18e6a
AlkSaints <meshacksuter2@gmail.com>
2022-12-13 03:51:42 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar followed by a newline
+ *
+ * return: Always 0(success)
+ */
+int main(void)
+{
+	char _putchar;
+
+	printf("%c\n", _putchar);
+	return (0);
+}
