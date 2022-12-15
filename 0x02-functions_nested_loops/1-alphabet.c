040cca0d91ee4b826ffe4304badc25bb602a4246
AlkSaints <meshacksuter2@gmail.com>
2022-12-14 23:37:58 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: void
+ */
+int print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
