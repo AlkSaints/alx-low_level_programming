985b6c82f27a22d2dff7486c102491411cc308d3
AlkSaints <meshacksuter2@gmail.com>
2022-12-15 18:38:53 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_triangle(int size);
 int _isupper(int c);
 
 #endif /* MAIN_H */
