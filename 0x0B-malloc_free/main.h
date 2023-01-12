8d24b7e15f6af714aeb5d6b3888ca9e285521fa4
AlkSaints <meshacksuter2@gmail.com>
2023-01-11 18:55:30 -0800

    malloc

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+
+#endif /* MAIN_H */
